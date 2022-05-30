6781fb061048814cf5b7472a7a9a27474bbe1047
SongJungHyun1004 <201802112@o.cnu.ac.kr>
2022-05-30 10:06:38 +0900

    initial commit

---
+++ parser/parser.mly
@@ -0,0 +1,57 @@
+%{
+  open Ast
+%}
+%token <int> NUMBER
+%token <string> IDENT
+%token PLUS MINUS LPAREN RPAREN LET IN EQ NEG FUN ARROW IF THEN ELSE ENDIF 
+%token TRUE FALSE LANGLE
+%token EOF
+%nonassoc low
+%left LANGLE
+%left PLUS MINUS 
+%nonassoc NUMBER NEG LPAREN LET IDENT IF TRUE FALSE
+%nonassoc mid
+%nonassoc high
+%type <unit> unop
+%type <Ast.expr> expr
+%type <Ast.expr list> args
+%type <string list> IDENT+
+%start <Ast.expr> parse
+%%
+
+parse: 
+  | e=expr EOF { e }
+  ;
+expr:
+  | n=NUMBER { Num n }
+  | TRUE { Bool true }
+  | FALSE { Bool false }
+  | unop n=NUMBER { Num (-1 * n) }
+  | NEG LPAREN e=expr RPAREN { Sub (Num 0, e) }
+  | LPAREN e=expr RPAREN { e }
+  | e1=expr PLUS e2=expr { Add (e1, e2) }
+  | e1=expr MINUS e2=expr { Sub (e1, e2) }
+  | e1=expr LANGLE e2=expr { LessThan (e1, e2) }
+  | IF e1=expr THEN e2=expr ELSE e3=expr ENDIF { Cond (e1, e2, e3) }
+  | LET x=IDENT EQ e1=expr IN e2=expr %prec low { LetIn (x, e1, e2) }
+  | LET x=IDENT xl=IDENT+ EQ e1=expr IN e2=expr %prec low { 
+    let ld = List.fold_right (fun x en -> Lambda (x, en)) xl e1 in
+    LetIn (x, ld, e2) 
+  }
+  | x=IDENT { Id x }
+  | e=expr el=args %prec mid { 
+    match List.rev (e :: el) with
+    | [] -> failwith "Unreachable"
+    | h :: t -> 
+        List.fold_right (fun e en -> App (e, en)) (List.rev t) h
+  }
+  | LPAREN FUN xl=IDENT+ ARROW e=expr RPAREN { 
+    List.fold_right (fun x en -> Lambda (x, en)) xl e
+  }
+  ;
+args:
+  | e=expr %prec high { [e] }
+  | el=args e=expr %prec high { el @ [e] }
+  ;
+unop:
+  | MINUS { () }
---
+++ store.ml
@@ -0,0 +1,46 @@
+module F = Format
+
+type t = (string * value) list
+and value = 
+        | NumV of int
+        | BoolV of bool 
+        | ClosureV of string * Ast.expr * t
+
+let rec pp_value fmt (v: value) : unit =
+        match v with
+        | NumV n -> F.fprintf fmt "%d" n
+        | BoolV b -> F.fprintf fmt "%b" b
+        | ClosureV (x, e, s) -> F.fprintf fmt "<λ%s. %a, %a>" x Ast.pp e pp_store s
+and pp_store fmt (s: t) : unit =
+        F.fprintf fmt "[%a]"
+        (F.pp_print_list ~pp_sep:(fun fmt () -> F.fprintf fmt "; ")
+        (fun fmt (x, v) -> F.fprintf fmt "%s -> %a" x pp_value v)) s
+
+let empty = []
+let mem (x:string) (s:t) : bool =
+        let rec is_mem (x:string) (s:t) : bool =
+                match s with
+                | [] -> false
+                | (var, _)::lst -> if var = x then true else is_mem x lst 
+        in
+        is_mem x s
+let add (x:string) (v: value) (s:t) : t =
+        let rec adder (x:string) (v:value) (s:t) (len:int) : t =
+                if len = 0 then (x, v) :: s
+                else
+                        match s with
+                        | [] -> []
+                        | (var, value)::lst -> if var = x then adder x v lst (len-1)
+                                                else adder x v (lst @ [(var, value)]) (len-1)
+        in
+        adder x v s (List.length s)
+
+let find (x:string) (s:t) : value =
+        let rec get (x:string) (s:t) (len:int) : value =
+                if len = 0 then failwith ("Free identifier: " ^ x)
+                else
+                        match s with
+                        | [] -> failwith ("Free identifier: " ^ x)
+                        | (var, value)::lst -> if var = x then value else get x lst (len-1)
+        in
+        get x s (List.length s)
